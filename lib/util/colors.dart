e30a7e5f39fa25aef3ca53df34bb31eba5e009b6
Ahmed Salah <ahm4dsalah@gmail.com>
2025-04-16 23:21:15 +0300

    Update colors.dart
    minor change

--- lib/util/colors.dart
+++ lib/util/colors.dart
@@ -4,13 +4,5 @@ class appcolor {
   static const Color primary = Colors.white;
   static const Color secondary = Color(0xFF070000);
   static const Color backgroundColor = Color(0xFF18D618);
-  static const double regular = 8.0;
-  static const double extra = 16.0;
-  final TextStyle appbarStyle = TextStyle(
-    color: backgroundColor,
-    fontSize: 24.0,
-    fontWeight: FontWeight.w600,
-  );
-  static get regularPadding => const EdgeInsets.all(regular);
-  static get extraPadding => const EdgeInsets.all(extra);
+
 }
