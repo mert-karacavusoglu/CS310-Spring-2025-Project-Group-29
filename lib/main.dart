2c081ac8247d4141d422acbe9c272e7e92412f5f
mert-karacavusoglu <mert.karacavusoglu@sabanciuniv.edu>
2025-04-17 19:39:28 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/routes/wishlist.dart';
+import 'package:shopping_app/routes/cart.dart';
 
 void main() {
   runApp(MaterialApp(
       initialRoute: '/wishlist',
       routes: {
-        '/wishlist': (context) => Wishlist()
+        '/wishlist': (context) => Wishlist(),
+        '/cart': (context) => Cart()
       },
       theme: ThemeData(fontFamily: 'serif') // text font from asset
   ));
