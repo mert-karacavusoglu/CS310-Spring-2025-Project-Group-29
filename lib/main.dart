500f346d8a8cd0196940a57dca923ea7991d0a8f
mert-karacavusoglu <mert.karacavusoglu@sabanciuniv.edu>
2025-04-17 22:35:42 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/routes/wishlist.dart';
 import 'package:shopping_app/routes/cart.dart';
+import 'package:shopping_app/routes/search.dart';
 
 void main() {
   runApp(MaterialApp(
       initialRoute: '/wishlist',
       routes: {
         '/wishlist': (context) => Wishlist(),
-        '/cart': (context) => Cart()
+        '/cart': (context) => Cart(),
+        '/search': (context) => SearchScreen()  
       },
       theme: ThemeData(fontFamily: 'serif') // text font from asset
   ));
