3aedec548b14a0c0c6fa6efa33efe681cfe4c277
mert-karacavusoglu <mert.karacavusoglu@sabanciuniv.edu>
2025-05-20 20:52:46 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:shopping_app/routes/userprofile.dart';
 import 'package:shopping_app/routes/settings.dart';
 import 'package:shopping_app/routes/home.dart';
 import 'package:shopping_app/routes/splash.dart';
+import 'package:shopping_app/model/auth_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -17,18 +18,35 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MaterialApp(
-      initialRoute: '/splash',
-      routes: {
-        '/splash' : (context) => SplashScreen(),
-        '/home' : (context) => HomeScreen(),
-        '/wishlist': (context) => Wishlist(),
-        '/cart': (context) => Cart(),
-        '/search': (context) => SearchScreen(),
-        '/userProfile':(context) => UserProfile(),
-        '/settings':(context) => SettingsPage(),
-        '/login': (context) => Login()
-      },
-      theme: ThemeData(fontFamily: 'serif') // text font from asset
-  ));
+  runApp(ShoppingApp());
+      
+
+}
+
+
+
+class ShoppingApp extends StatelessWidget {
+  const ShoppingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => AuthInfoProvider(),
+      child: MaterialApp(
+        initialRoute: '/splash',
+        routes: {
+          '/splash' : (context) => SplashScreen(),
+          '/home' : (context) => HomeScreen(),
+          '/wishlist': (context) => Wishlist(),
+          '/cart': (context) => Cart(),
+          '/search': (context) => SearchScreen(),
+          '/userProfile':(context) => UserProfile(),
+          '/settings':(context) => SettingsPage(),
+          '/login': (context) => Login()
+        },
+        theme: ThemeData(fontFamily: 'serif') // text font from asset
+      ),
+    );
+  }
+
 }
